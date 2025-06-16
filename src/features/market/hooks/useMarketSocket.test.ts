25e1c900361253a200495e51d516263050c3153c
Luis Matos <luismatosrobot@gmail.com>
2025-06-16 02:52:38 -0300

    fix build files

--- src/features/market/hooks/useMarketSocket.test.ts
+++ src/features/market/hooks/useMarketSocket.test.ts
@@ -42,7 +42,6 @@ describe("useMarketSocket", () => {
       useMarketSocket(["btcusdt", "invalid", "ethusdt"])
     );
 
-    // aguarda o efeito assíncrono acontecer
     await new Promise((r) => setTimeout(r, 0));
 
     expect(global.WebSocket).toHaveBeenCalledWith(
